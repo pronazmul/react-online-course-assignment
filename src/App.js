2404cf0fbd9b8dca207d8fd8eeee3b89375d2b43
Nazmul Huda <mhnazmul48@gmail.com>
2020-08-24 13:40:13 +0600

    Get Specific Data event.target.name

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Form from './components/Form';
+import Form2 from './components/Form2';
 
 
 
 function App() {
   return (   
           <div>
+              <h3 className='text-center'>Get Date event.target.name</h3>
+              <Form2></Form2>
+              <hr></hr>
+              <h3 className='text-center'>Get Date event.target.value</h3>
               <Form></Form>
           </div>
    )
