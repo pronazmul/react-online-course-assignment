4b248d16ab64d93e51caf1a2b133833aca2f20af
Nazmul Huda <mhnazmul48@gmail.com>
2020-08-27 02:44:29 +0600

    React Online Course Project Done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import FormSingle from './components/FormSingle';
-import FormMultiple from './components/FormMultiple';
-import FormValidation from './components/FormValidation';
-import SelectOption from './components/SelectOption';
-import SelectOptionJson from './components/SelectOptionJson';
+import Course from './components/Course/Course';
 
 
 
 function App() {
-  return (   
-          <div>
-              <h3 className='text-center'>Form Validation Using Regular Expression</h3>
-              <FormValidation></FormValidation>
-              <hr></hr>
-              <h3 className='text-center'>event.target.name and event.target.value</h3>
-              <FormMultiple></FormMultiple>
-              <hr></hr>
-              <h3 className='text-center'>event.target.value</h3>
-              <FormSingle></FormSingle>
-              <hr></hr>
-              <h3 className='text-center'>event.target.value</h3>
-              <SelectOption></SelectOption>
-              <hr></hr>
-              <h3 className='text-center'>Select Option From json.map</h3>
-              <SelectOptionJson></SelectOptionJson>
-          </div>
+  return ( 
+          <Course></Course>
    )
 }
 
---
+++ src/components/Course/Course.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import DemoCourse from '../../DemoCourse'
+import Banner from '../Banner/Banner';
+import { hydrate } from 'react-dom';
+import CourseCard from '../CourseCard/CourseCard';
+import Cart from '../Cart/Cart';
+
+const Course = () => {
+
+    const [cart, setCart] = useState([])
+    
+    const mannageCart =(item)=>{
+        const newAdded =[...cart, item]
+        setCart(newAdded)
+    }
+    const totalPrice = cart.reduce((initial, course)=> initial + course.price,0)
+        return (
+            <div className='container-fluid bg-color'>
+                <Banner length={DemoCourse.length}></Banner>
+                <div className="container pt-5">
+                    <div className="row">
+                        <div className="col-md-9">
+                            <h2 className='text-color text-center font-weight-bold'>All Courses</h2><br></br>
+                            <div className="row pb-5">
+                                {DemoCourse.map(course=><CourseCard key={course.title} course={course} mannageCart={mannageCart}></CourseCard>)}
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <h2 className='text-color text-center font-weight-bold'>Cart</h2><br></br>
+                            <ul className='list-group'>
+                                {
+                                    cart.map(course=> <Cart course={course}></Cart>)
+                                }
+                            </ul>
+                            <h4 className='text-center card-bg py-2 rounded mt-3'> Check Out: $ {totalPrice} </h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+};
+
+export default Course;
